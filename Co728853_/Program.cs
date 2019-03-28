f7412b96bd9c5b2de82031751078132ee70998bb
ajaydevsingh9592 <46795478+ajaydevsingh9592@users.noreply.github.com>
2019-03-28 16:47:54 -0400

    4:49 commit

--- Co728853_/Program.cs
+++ Co728853_/Program.cs
@@ -16,9 +16,10 @@ namespace c0728853
             Console.ReadLine();
         }
 
-        static void Download()
+        static async void Download()
         {
-            Network.Download();
+            await Network.Download();
+            Console.WriteLine("Download Complete"); 
         }
         class Network
         {
